3daf09ffa258bdf06cb68da4866841cf04f1434e
Margaree Peacocke <margaree.p@gmail.com>
2018-12-03 10:25:15 -0500

    add tests for siren-entity-loading.html

---
+++ test/siren-entity-loading.js
@@ -0,0 +1,39 @@
+/* global suite, test, fixture, expect, setup, teardown, sinon */
+
+'use strict';
+
+suite('siren-entity-loading', function() {
+	var element, sandbox;
+
+	setup(function() {
+		sandbox = sinon.sandbox.create();
+		element = fixture('basic');
+	});
+
+	teardown(function() {
+		sandbox.restore();
+	});
+
+	['loading', 'error', 'fetched'].forEach(function(state) {
+		suite(state, function() {
+			[true,false].forEach(function(elementEnabled) {
+				test(`should ${elementEnabled ? '' : 'not'} show ${state} content when ${elementEnabled ? '' : 'not'} ${state}`, function(done) {
+					if (state !== 'loading') {
+						element.loading = false;
+					}
+					element[state] = elementEnabled;
+
+					setTimeout(function() {
+						var div = element.$$(`.${state}`);
+						expect(div.classList.contains('show')).to.equal(elementEnabled);
+						if (state === 'loading') {
+							expect(div.classList.contains('hidden')).to.equal(!elementEnabled);
+						}
+
+						done();
+					}, 600);
+				});
+			});
+		});
+	});
+});
